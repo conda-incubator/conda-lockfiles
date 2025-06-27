c7cd5b4617b188b769aab097f7882b15a39c8c24
Jonathan Helmus <jjhelmus@gmail.com>
2025-06-27 11:12:24 -0500

    raise exception if export to pixi is not possible

--- conda_lockfiles/dumpers/rattler_lock.py
+++ conda_lockfiles/dumpers/rattler_lock.py
@@ -8,15 +8,22 @@ from conda.base.context import context
 from conda.core.prefix_data import PrefixData
 from ruamel.yaml import YAML
 
+from ..exceptions import EnvironmentExportNotSupported
+
 if TYPE_CHECKING:
     from typing import Any
 
     from conda.models.records import PackageRecord
 
 
+LOCKFILE_FORMAT = "pixi"
+
+
 def _record_to_rattler_lock_v6(record: PackageRecord) -> dict[str, Any]:
+    if record.url is None:
+        raise EnvironmentExportNotSupported(LOCKFILE_FORMAT)
     package = {
-        "conda": record.url,
+        "conda": str(record.url),
     }
     # add relevent non-empty fields that rattler_lock includes in v6 lockfiles
     # https://github.com/conda/rattler/blob/rattler_lock-v0.23.5/crates/rattler_lock/src/parse/models/v6/conda_package_data.rs#L46
@@ -45,14 +52,17 @@ def _record_to_rattler_lock_v6(record: PackageRecord) -> dict[str, Any]:
 
 def export_to_rattler_lock_v6(prefix: str, lockfile_path: str | None) -> None:
     prefix_data = PrefixData(prefix)
-    channel_urls = {(record.channel) for record in prefix_data.iter_records()}
-    channels = [{"url": str(url)} for url in channel_urls]
-    env_subdir_pkgs = [
-        {"conda": str(record.url)} for record in prefix_data.iter_records()
-    ]
-    packages = [
-        _record_to_rattler_lock_v6(record) for record in prefix_data.iter_records()
-    ]
+    unique_channels = {(record.channel) for record in prefix_data.iter_records()}
+    if None in unique_channels:
+        raise EnvironmentExportNotSupported(LOCKFILE_FORMAT)
+    channels = sorted([{"url": str(url)} for url in unique_channels])
+    if None in channels:
+        raise Exception
+    packages = sorted(
+        [_record_to_rattler_lock_v6(record) for record in prefix_data.iter_records()],
+        key=lambda x: x["conda"],
+    )
+    env_subdir_pkgs = [{"conda": package["conda"]} for package in packages]
     environments = {
         "default": {
             "channels": channels,
