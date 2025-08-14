a3a428d83d6f272e93838158ee1e6565d6406776
kenodegard <kodegard@anaconda.com>
2025-08-13 23:25:42 -0500

    Remove subcommand

--- conda_lockfiles/plugin.py
+++ conda_lockfiles/plugin.py
@@ -2,27 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from conda.plugins import (
-    CondaEnvironmentExporter,
-    CondaEnvironmentSpecifier,
-    CondaSubcommand,
-    hookimpl,
-)
+from conda.plugins import CondaEnvironmentExporter, CondaEnvironmentSpecifier, hookimpl
 
-from . import cli, dumpers, loaders
+from . import dumpers, loaders
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
-
-
-@hookimpl
-def conda_subcommands():
-    yield CondaSubcommand(
-        name="lockfiles",
-        summary="Create new environments from different conda ecosystem lockfiles",
-        action=cli.execute,
-        configure_parser=cli.configure_parser,
-    )
 
 
 @hookimpl
