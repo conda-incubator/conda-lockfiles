03b6114583b49f84ec8c00a9cf3c0b72f5c152bb
kenodegard <kodegard@anaconda.com>
2025-08-18 14:09:34 -0500

    Drop plugin_manager mocking

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,3 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
 import pytest
 from conda.plugins import reporter_backends
 from conda.plugins.hookspec import CondaSpecs
@@ -9,17 +5,13 @@ from conda.plugins.manager import CondaPluginManager
 
 import conda_lockfiles.plugin
 
-if TYPE_CHECKING:
-    from pytest_mock import MockerFixture
-
 pytest_plugins = ("conda.testing.fixtures",)
 
 
 @pytest.fixture
-def plugin_manager(mocker: MockerFixture) -> CondaPluginManager:
+def plugin_manager() -> CondaPluginManager:
     manager = CondaPluginManager()
     manager.add_hookspecs(CondaSpecs)
-    mocker.patch("conda.plugins.manager.get_plugin_manager", return_value=manager)
     # since we invoke progress bars we need the reporter backends
     manager.load_plugins(*reporter_backends.plugins)
     manager.load_plugins(conda_lockfiles.plugin)
