e7ee9a4611d138f5524a6428c85e27b05bf969ce
kenodegard <kodegard@anaconda.com>
2025-08-18 14:08:11 -0500

    Compare parsed yaml

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,13 @@
+from __future__ import annotations
+
 import re
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+from conda.common.serialize import yaml_safe_load
+
+if TYPE_CHECKING:
+    from typing import Any
 
 DATA_DIR = Path(__file__).parent / "data"
 
@@ -22,8 +30,8 @@ RE_CREATED_BY = re.compile(r"created_by: conda-lockfiles .+")
 RE_CREATED_AT = re.compile(r"created_at: .+")
 
 
-def normalize_conda_lock_v1(lockfile: Path) -> str:
+def normalize_conda_lock_v1(lockfile: Path) -> Any:
     text = lockfile.read_text()
     text = RE_CREATED_BY.sub("created_by: conda-lockfiles VERSION", text)
     text = RE_CREATED_AT.sub("created_at: TIMESTAMP", text)
-    return text
+    return yaml_safe_load(text)
